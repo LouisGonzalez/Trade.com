3f54f1ed9918541beb24f04a23e5fd6009877ad5
Yeferal <yefercuber20@gmail.com>
2021-10-02 18:21:33 -0600

    se agrega los componentes, login,signin,signup y home-page

--- View/src/app/app.module.ts
+++ View/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
